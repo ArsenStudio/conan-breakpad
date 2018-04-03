8504bfc52bdaeb1a26e52715612bc2c95441fdc8
intelligide <intelligide@hotmail.fr>
2018-04-03 07:52:18 +0000

    Update build.py for clang support

--- build.py
+++ build.py
@@ -3,4 +3,8 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["compiler"] == "clang":
+            env_vars["CC"] = "clang"
+            env_vars["CXX"] = "clang++"
     builder.run()

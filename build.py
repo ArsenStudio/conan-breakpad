02794448518ad5f84144cb677c44bec700873d0d
Intelligide <intelligide@hotmail.fr>
2018-04-14 10:23:14 +0200

    Upload only when stable

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
-    for settings, options, env_vars, build_requires in builder.items:
+    for settings, options, env_vars, build_requires, reference in builder.items:
         if settings["compiler"] == "clang":
             env_vars["CC"] = "clang"
             env_vars["CXX"] = "clang++"
